ff7bad8f8c42d59365f126b7d9e0197a33e1ac2e
Sanjeev Pandey <sanjeev.pandey@jupiter.money>
2020-07-15 17:09:30 +0530

    fluentbit added

---
+++ fluentbit/entrypoint.sh
@@ -0,0 +1,3 @@
+echo -n "AWS for Fluent Bit Container Image Version "
+cat /AWS_FOR_FLUENT_BIT_VERSION
+exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -e /fluent-bit/out_coralogix.so -c /fluent-bit/etc/fluent-bit.conf
